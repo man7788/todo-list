42b422127868b37b1e6528d7d400a711c0c97e0b
man7788 <jerman1841@gmail.com>
2022-10-29 02:28:12 +0800

    Add webpack config

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './style.css';
+
+function component() {
+  const element = document.createElement('div');
+  element.textContent = 'Hello World!';
+  return element;
+}
+
+document.body.appendChild(component());
